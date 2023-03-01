c8e2a5504693a845fbeb35041b189e2ed8875e05
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-03-01 17:47:34 +0530

    Add files via upload

---
+++ Practical/TestStudent.java
@@ -0,0 +1,22 @@
+package corejava;
+
+public class TestStudent 
+{
+
+	public static void main(String[] args) 
+	{
+		//printing student data line 
+		System.out.println("Student data"+"\n");
+		//create a variable to printing student data
+		StudentData s1=new StudentData();
+		//use the variable of creating student data
+		s1.setStudent_id(1);
+		s1.setStudent_name("Shaheen");
+		s1.setStudent_age(21);
+		//printing the data
+		System.out.println("Student ID:"+s1.getStudent_id()+"\n"+"Student Name:"+s1.getStudent_name()+"\n"+"Student Age:"+s1.getStudent_age());
+		
+
+	}
+
+}
