0cc6eb8a7bb61ce13f7ac788f71a8319f3bd547f
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-03-01 17:11:23 +0530

    Add files via upload

---
+++ Practical/StudentData.java
@@ -0,0 +1,31 @@
+//create a student class with fields id,name,age.invoke getters and setters and print the output in the form
+
+package corejava;
+
+public class StudentData
+{
+	// create variables of data members 
+	private int student_id;
+	private String student_name;
+	private int student_age;
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	public String getStudent_name() {
+		return student_name;
+	}
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+	public int getStudent_age() {
+		return student_age;
+	}
+	public void setStudent_age(int student_age) {
+		this.student_age = student_age;
+	}
+	
+	
+}
