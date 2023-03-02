13e32fdae6188f2c0b59e7588c06348723644694
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-03-03 01:48:06 +0530

    Add files via upload

---
+++ Practical/AbstractionTest.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class AbstractionTest {
+
+	public static void main(String[] args) 
+	{
+		AbstractionData ad=new AbstractionData(1,"Gaytri","Pune",15000.00);
+		ad.mailCheck();
+		
+
+	}
+
+}
