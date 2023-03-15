333b9e01c35bc8af85b78eb27880df54b569abc4
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-03-15 18:16:37 +0530

    Add files via upload

---
+++ Practical/AssignmentOpr.java
@@ -0,0 +1,29 @@
+package Operatorss;
+
+import java.util.Scanner;
+
+public class AssignmentOpr
+{
+
+	public static void main(String[] args) 
+	{
+		//create Scanner class to user input
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number: ");
+	    int num1 = sc.nextInt();
+
+	    System.out.print("Enter another number: ");
+	    int num2 = sc.nextInt();
+
+	    System.out.println("Initial value of num1: " + num1);
+	    System.out.println("Initial value of num2: " + num2);
+
+	    // Assignment operators example
+	    num1 += num2;
+	    num2 -= num1;
+	    System.out.println("Final value of num1: " + num1);
+	    System.out.println("Final value of num2: " + num2);
+	    sc.close();
+	}
+
+}
