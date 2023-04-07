92b02a3ea73101a61bf6b64cd82df4528cc5e947
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-04-07 17:03:45 +0530

    Add files via upload

---
+++ Practical/TreeSetEx.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.*;
+
+public class TreeSetEx
+{
+
+	public static void main(String[] args) 
+	{
+		Set<String> ts =new TreeSet<String>();
+		//add elements
+		ts.add("B");
+		ts.add("A");
+		ts.add("C");
+		ts.add("B");
+		//printing the elements
+		System.out.println("Elements are:" +ts);
+		 
+	}
+
+}
