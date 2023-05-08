75fb36e606ec69ebee4a6b74d644eacd2149222b
shaheen812 <126570535+shaheen812@users.noreply.github.com>
2023-05-08 19:01:43 +0530

    Add files via upload

--- Project/App.java
+++ Project/App.java
@@ -1,24 +1,13 @@
-package com.management.main;
+package com.example.users;
 
-import com.management.dao.BloodDao;
-import com.management.model.Blood;
-
-public class App
+/**
+ * Hello world!
+ *
+ */
+public class App 
 {
-
-	public static void main(String[] args) 
-	{
-		BloodDao dao = new BloodDao();	    
-	    // Create a new user entity
-	    Blood blood = new Blood();
-	    blood.setName("John Smith");
-	    blood.setBloodGroup("B+");
-	   dao.saveBlood(blood);
-	    
-	    blood.setName("abc xyz");
-	    blood.setBloodGroup("AB+");
-	    dao.updateUser(blood);
-	    dao.deleteBlood(blood);
-	}
-
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
